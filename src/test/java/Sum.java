87e6056d5fc493ee277a55b74e10190965aebda4
Ruslan Munasipov <ruslanmunasipov23@gmail.com>
2021-10-11 15:50:47 +0300

    Complete work

--- src/test/java/Sum.java
+++ src/test/java/Sum.java
@@ -15,50 +15,63 @@ public class Sum {
     String name = "Ruslan",
             lastName = "Munasipov",
             eMail = "ruslanmunasipov23@gmail.com",
-            phone = "79991845170",
-            dateOfBirth = "23 feb 1994",
-            subjects = "java, automationQA",
-            currentAddress = "Saratov";
+            phone = "9991845170",
+            dateOfBirth = "1994",
+            subjects = "Maths",
+            currentAddress = "Saratov",
+            state = "Rajasthan",
+            city = "Jaipur",
+            assertName = "Ruslan Munasipov",
+            assertEMail = "ruslanmunasipov23@gmail.com",
+            assertGender = "Male",
+            assertPhone = "9991845170",
+            assertDateOfBirth = "23 February,1994",
+            assertSubjects = "Maths",
+            assertHobbies = "Sports",
+            assertPicture = "123.jpg",
+            assertCurrentAddress = "Saratov",
+            assertStateCity = "Rajasthan Jaipur";
 
-//    xPaths
-//    First Name: //div[@class = 'col-md-4 col-sm-6']//input[@placeholder = 'First Name']
-//    Last Name: //div[@class = 'col-md-4 col-sm-6']//input[@placeholder = 'Last Name']
-//    Email: //div[@class = 'col-md-9 col-sm-12']//input[@placeholder = 'name@example.com']
-//    Male Gender: //label[@for = 'gender-radio-1']
-//    Phone: //div[@class = 'col-md-9 col-sm-12']//input[@id = 'userNumber']
-//    DateOfBirth: //select[@class = 'react-datepicker__month-select']/option[@value = '1'] - February
-//    Subjects: //div[@class = 'subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3']
-//    HobbiesSport: //label[@for = 'hobbies-checkbox-1']
-//    UploadPicture: //label[@for = 'uploadPicture']
-//    CurrentAddress: //div[@class = 'col-md-9 col-sm-12']//textarea[@class = 'form-control']
-//    SelectState: //div[@class = ' css-yk16xz-control']
-//    SelectCity: //div[@class = ' css-14jk2my-container']
-//    Submit: //button[text() = 'Submit']
 
     @Test
     public void main() throws InterruptedException {
         open("https://demoqa.com/automation-practice-form");
+
+
+        //заполняем
+
+
         $x("//div[@class = 'col-md-4 col-sm-6']//input[@placeholder = 'First Name']").setValue(name);
         $x("//div[@class = 'col-md-4 col-sm-6']//input[@placeholder = 'Last Name']").setValue(lastName);
         $x("//div[@class = 'col-md-9 col-sm-12']//input[@placeholder = 'name@example.com']").setValue(eMail);
         $x("//label[@for = 'gender-radio-1']").click();
         $x("//div[@class = 'col-md-9 col-sm-12']//input[@id = 'userNumber']").setValue(phone);
         $x("//div[@class = 'react-datepicker__input-container']//input").click();
-        $x("//select[@class = 'react-datepicker__month-select']").click();
         $x("//select[@class = 'react-datepicker__month-select']").selectOptionByValue("1");
-        $x("//div[@class = 'react-datepicker__year-dropdown-container react-datepicker__year-dropdown-container--select']").click();
-        $x("//div[@class = 'react-datepicker__year-dropdown-container react-datepicker__year-dropdown-container--select']").selectOption("1994");
-        $x("//div[@class = 'react-datepicker__day react-datepicker__day--023']").selectOptionByValue("23");
-        $x("//div[@class = 'subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3']").click();
+        $x("//select[@class='react-datepicker__year-select']").selectOption(dateOfBirth);
+        $x("//div[@class = 'react-datepicker__day react-datepicker__day--023']").click();
+        $x("//div[@class = 'subjects-auto-complete__input']//input[@id = 'subjectsInput']").setValue(subjects).pressEnter();
         $x("//label[@for = 'hobbies-checkbox-1']").click();
-        //$x("//label[@for = 'uploadPicture']").click();
+        $x("//input[@id = 'uploadPicture']").sendKeys("C:\\Users\\лвыао\\IdeaProjects\\Yandex Test\\src\\test\\123.jpg");
         $x("//div[@class = 'col-md-9 col-sm-12']//textarea[@class = 'form-control']").setValue(currentAddress);
-        //$x("//div[@class = ' css-yk16xz-control']").setValue("sar");
-        //$x("//div[@class = ' css-14jk2my-container']").setValue("Saratov");
+        $x("//input[@id = 'react-select-3-input']").setValue(state).pressEnter();
+        $x("//input[@id = 'react-select-4-input']").setValue(city).pressEnter();
         $x("//button[text() = 'Submit']").click();
-        $x("//button[text() = 'Submit']").click();
-        Assertions.assertEquals($x("//div[@class = 'col-md-4 col-sm-6']//input[@placeholder = 'First Name']").getText(), name, "nashel");
-        Assertions.assertEquals($x("//div[@class = 'col-md-4 col-sm-6']//input[@placeholder = 'Last Name']").getText(), lastName, "nashel");
+
+
+        //проверяем
+
+
+        Assertions.assertEquals($x("//table[@class = 'table table-dark table-striped table-bordered table-hover']//td[.= 'Ruslan Munasipov']").getText(), assertName, "nashel");
+        Assertions.assertEquals($x("//table[@class = 'table table-dark table-striped table-bordered table-hover']//td[.= 'ruslanmunasipov23@gmail.com']").getText(), assertEMail, "nashel");
+        Assertions.assertEquals($x("//table[@class = 'table table-dark table-striped table-bordered table-hover']//td[.= 'Male']").getText(), assertGender, "nashel");
+        Assertions.assertEquals($x("//table[@class = 'table table-dark table-striped table-bordered table-hover']//td[.= '9991845170']").getText(), assertPhone, "nashel");
+        Assertions.assertEquals($x("//table[@class = 'table table-dark table-striped table-bordered table-hover']//td[.= '23 February,1994']").getText(), assertDateOfBirth, "nashel");
+        Assertions.assertEquals($x("//table[@class = 'table table-dark table-striped table-bordered table-hover']//td[.= 'Maths']").getText(), assertSubjects, "nashel");
+        Assertions.assertEquals($x("//table[@class = 'table table-dark table-striped table-bordered table-hover']//td[.= 'Sports']").getText(), assertHobbies, "nashel");
+        Assertions.assertEquals($x("//table[@class = 'table table-dark table-striped table-bordered table-hover']//td[.= '123.jpg']").getText(), assertPicture, "nashel");
+        Assertions.assertEquals($x("//table[@class = 'table table-dark table-striped table-bordered table-hover']//td[.= 'Saratov']").getText(), assertCurrentAddress, "nashel");
+        Assertions.assertEquals($x("//table[@class = 'table table-dark table-striped table-bordered table-hover']//td[.= 'Rajasthan Jaipur']").getText(), assertStateCity, "nashel");
     }
 }
 
